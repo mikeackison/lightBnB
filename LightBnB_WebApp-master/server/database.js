bba8cb22219cc4f86f37b1c096c3ce95a4686fb0
Mike Ackison <mike.ackison@shopify.com>
2021-02-05 00:49:59 +0000

    add SQL queries to getAllProperties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -145,21 +145,61 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  // make sure to return
-  return pool.query(`
-    SELECT * FROM properties
-    LIMIT $1
-  `, [limit])
 
-  // return a promise that will result in res.rows
-    .then(res => (res.rows)
-    );
-  // const limitedProperties = {};
-  // for (let i = 1; i <= limit; i++) {
-  //   limitedProperties[i] = properties[i];
-  // }
-  // return Promise.resolve(limitedProperties);
+  // 1 Setup an array to hold any parameters that may be available for the query.
+  const queryParams = [];
+
+  // 2 Start the query with all information that comes before the WHERE clause.
+  let queryString = `
+  SELECT properties.*, avg(property_reviews.rating) as average_rating
+  FROM properties
+  JOIN property_reviews ON properties.id = property_id
+  WHERE TRUE
+   `;
+
+  // 3 Check if a city has been passed in as an option.
+  //  Add the city to the params array and create a WHERE clause for the city.
+  // We can use the length of the array to dynamically get the $n placeholder number.
+  if (options.city) {
+    queryParams.push(`%${options.city}%`);
+    queryString += `AND city LIKE $${queryParams.length} `;
+  }
+
+  if (options.owner_id) {
+    queryParams.push(`${options.owner_id}`);
+    queryString += `AND owner_id = $${queryParams.length}`;
+  }
+
+  if (options.minimum_price_per_night && options.maximum_price_per_night) {
+    queryParams.push(options.minimum_price_per_night * 100, options.maximum_price_per_night * 100);
+    queryString += `AND cost_per_night >= $${queryParams.length - 1 } AND cost_per_night <= $${queryParams.length}`;
+  }
+
+  // add the GROUP BY properties.id to the string
+  queryString += `
+  GROUP BY properties.id
+  `;
+
+  if (options.minimum_rating) {
+    queryParams.push(options.minimum_rating);
+    queryString += `HAVING AVG(property_reviews.rating) >= $${queryParams.length}`;
+  }
+
+  
+  // push the limi of 10 at the end
+  queryParams.push(limit);
+  queryString += `
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+  // 5 Console log everything just to make sure we've done it right.
+  console.log("QUERY STRING", queryString, "AND QUERY PARAMS", queryParams);
+
+  // 6 Run the query.
+  return pool.query(queryString, queryParams)
+    .then(res => res.rows);
 };
+
 exports.getAllProperties = getAllProperties;
 
 
@@ -175,3 +215,4 @@ const addProperty = function(property) {
   return Promise.resolve(property);
 };
 exports.addProperty = addProperty;
+
